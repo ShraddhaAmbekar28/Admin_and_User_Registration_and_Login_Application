dc0974b5090ddf7d9e4325490aa9c22ff61ed3f0
ShraddhaAmbekar28 <161967232+ShraddhaAmbekar28@users.noreply.github.com>
2024-06-25 15:18:49 +0530

    Add files via upload

---
+++ Admin_User_Registration/src/test/java/admin_user/Amin_and_User_registration_and_loginApplication.java
@@ -0,0 +1,13 @@
+package admin_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amin_and_User_registration_and_loginApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
